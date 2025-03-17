a7556c51f1c67ff67683cd716697d6563e134f80
therealpeppy <furghadmanar@gmail.com>
2025-03-17 16:52:36 -0500

    * + Draw Function * + Fill Function * + key Class - get_pressed() & get_focused() functions

--- e.js
+++ e.js
@@ -1,20 +1,96 @@
+class key {
+    constructor() {
+        this.type = "key";
+        this.dataType = "key";
+        this.pressedKeys = {};
+        this.focused = true;
+
+        document.addEventListener("keydown", (event) => {
+            this.pressedKeys[event.key] = true;
+        });
+
+        document.addEventListener("keyup", (event) => {
+            this.pressedKeys[event.key] = false;
+        });
+
+        window.addEventListener("focus", () => {
+            this.focused = true;
+        });
+
+        window.addEventListener("blur", () => {
+            this.focused = false;
+        });
+    }
+
+    get_pressed() {
+        return this.pressedKeys;
+    }
+
+    get_focused() {
+        return this.focused;
+    }
+}
+
 class colorRGBA {
-	constructor(R = 0, G = 0, B = 0, A = 1) {
-		this.r = R;
-		this.g = G;
-		this.b = B;
-		this.a = A;
+	constructor(R = 255, G = 255, B = 255, Transparency = 1) {
+		this.R = R;
+		this.G = G;
+		this.B = B;
+		this.A = (Transparency - 1) * -1;
+		this.type = "colorRGBA";
+		this.dataType = "color";
 	}
 }
 
 class Rect {
-	constructor(px, py, sx, sy, fillColor = new colorRGBA()) {
-		this.px = px;
-		this.py = py;
-		this.sx = sx;
-		this.sy = sy;
+	constructor(pX, pY, sW, sH, fillColor = new colorRGBA()) {
+		this.pX = pX;
+		this.pY = pY;
+		this.sW = sW;
+		this.sH = sH;
 		this.fillColor = fillColor;
+		this.type = "Rect";
+		this.dataType = "DrawableObject";
 	}
 }
 
-export { colorRGBA, Rect };
+class Circle {
+	constructor(pX, pY, R, fillColor = new colorRGBA()) {
+		this.pX = pX;
+		this.pY = pY;
+		this.radius = R;
+		this.fillColor = fillColor;
+		this.type = "Circle";
+		this.dataType = "DrawableObject";
+	}
+}
+
+function Draw(object){
+	if (object.dataType == "DrawableObject") {
+		if (object.type == "Rect") {
+			let body = document.querySelector("body")
+			let drawn = document.createElement("div")
+			drawn.style.width = `${object.sW}px`
+			drawn.style.height = `${object.sH}px`
+			drawn.style.position = `absolute`
+			drawn.style.left = `${object.pX}px`
+			drawn.style.top = `${object.pY}px`
+			drawn.style.backgroundColor = `rgba(${object.fillColor.R}, ${object.fillColor.G}, ${object.fillColor.B}, ${object.fillColor.A})`
+			drawn.classList.add("object")
+			body.appendChild(drawn)
+		} else if (object.type == "Circle") {
+
+		}
+	}
+}
+
+function Fill(fillColor = new colorRGBA()){
+	let body = document.querySelector("body")
+	let objects = document.getElementsByClassName("object")
+	for(let i = 0; i<objects.length; i++){
+		objects[i].remove()
+	}
+	body.style.backgroundColor = `rgba(${fillColor.R}, ${fillColor.G}, ${fillColor.B}, ${fillColor.A})`
+}
+
+export { colorRGBA, Rect, Circle, Draw, Fill};
--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-import { colorRGBA, Rect, Draw} from './e.js';
+import { colorRGBA, Rect, Draw, Fill} from './e.js';
 
-let testRect = new Rect(1, 1, 2, 2, new colorRGBA(255, 0, 0, 0))
+let run = false
+let testRect = new Rect(300, 200, 40, 40, new colorRGBA(255, 0, 0, 0))
 
 function gameLoop(){
-	Draw(testRect)
-	setTimeout(gameLoop, 1);
+	if (run) {
+		Fill(new colorRGBA())
+		Draw(testRect)
+		setTimeout(gameLoop, 10);
+	}
 }
+
+if (run){
+	gameLoop()
+}
+
+
+Fill(new colorRGBA())
+Draw(testRect)
