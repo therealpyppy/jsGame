31215455f5303ae2a2d41596d1a2c2ab335cc728
therealpeppy <furghadmanar@gmail.com>
2025-03-17 16:21:22 -0500

    Added test gameLoop

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-import { colorRGBA, Rect } from './e.js';
+import { colorRGBA, Rect, Draw} from './e.js';
 
-let testRect = new Rect()
-console.log(testRect)
+let testRect = new Rect(1, 1, 2, 2, new colorRGBA(255, 0, 0, 0))
+
+function gameLoop(){
+	Draw(testRect)
+	setTimeout(gameLoop, 1);
+}
