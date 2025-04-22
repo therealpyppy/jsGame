5759ef30896b017982176bf076c329dc0f09ba76
therealpeppy <furghadmanar@gmail.com>
2025-04-22 18:51:49 -0500

    Add optional params to draw.rect

--- src/draw/shapes/rect.js
+++ src/draw/shapes/rect.js
@@ -1,4 +1,6 @@
-export function drawRect(ctx, left, top, width, height, color) {
+export function rect(ctx, color, rect,
+	borderRadius = 0, borderTopLeftRadius = -1, borderTopRightRadius = -1, 
+borderBottomLeftRadius = -1, borderBottomRightRadius = -1) {
 	ctx.fillStyle = color;
 	ctx.fillRect(left, top, width, height);
 }
