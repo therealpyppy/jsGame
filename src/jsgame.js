af2fefd3aa2924a596c280811551bc369fed9723
therealpeppy <furghadmanar@gmail.com>
2025-04-22 19:47:35 -0500

    + base jsgame script

--- src/jsgame.js
+++ src/jsgame.js
@@ -1,5 +1,15 @@
-import { Rect } from "core/rect.js";
+import { core } from "./core/core.js";
+import { draw } from "./draw/draw.js";
 
-export class jsgame {
+export const jsgame = {
+	init() {
+		document.body.style.margin = "0px";
+		let canvas = document.querySelector("canvas");
+		if (canvas === null) {
+			document.body.appendChild(document.createElement("canvas"));
+		}
+	},
 
+	core,
+	draw
 };
