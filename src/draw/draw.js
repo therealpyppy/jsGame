ba9963041d39ffdbbbc42cc24576073c66f6cbb1
therealpeppy <furghadmanar@gmail.com>
2025-04-24 22:19:21 -0500

    added circle to draw index file

--- src/draw/draw.js
+++ src/draw/draw.js
@@ -1,7 +1,9 @@
 import { rect } from './shapes/rect.js';
 import { line } from './shapes/line.js';
+import { circle } from './shapes/circle.js';
 
 export const draw = {
 	rect,
-	line
+	line,
+	circle
 };
