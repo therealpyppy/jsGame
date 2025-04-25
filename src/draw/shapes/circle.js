f4e044c539a89e9e18359d0e475a9f134f1b7403
therealpeppy <furghadmanar@gmail.com>
2025-04-24 22:21:50 -0500

    reordered code to properly throw errors

--- src/draw/shapes/circle.js
+++ src/draw/shapes/circle.js
@@ -1,6 +1,8 @@
 import { core } from "../../core/core.js"
 
 export function circle(ctx, color, center, radius, width=0, draw_top_right=false, draw_top_left=false, draw_bottom_left=false, draw_bottom_right=false) {
+	ctx.fillStyle = new core.Color(color).hex3;
+	ctx.strokeStyle = new core.Color(color).hex3;
 	if (!Array.isArray(center) || center.length !== 2) {
 		throw new Error("center must be an array of two numbers [x, y]");
 	}
@@ -8,8 +10,6 @@ export function circle(ctx, color, center, radius, width=0, draw_top_right=false
 	let y = center[1];
 
 	ctx.lineWidth = width;
-	ctx.fillStyle = new core.Color(color).hex3;
-	ctx.strokeStyle = new core.Color(color).hex3;
 
 	if (draw_top_right, draw_top_left, draw_bottom_left, draw_bottom_right) {
 
