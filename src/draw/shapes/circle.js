73bf418c8cf7a567bdb25ee4f5fa1d73fc2de3be
therealpeppy <furghadmanar@gmail.com>
2025-04-24 22:09:27 -0500

    create empty draw.circle function

---
+++ src/draw/shapes/circle.js
@@ -0,0 +1,5 @@
+import { core } from "../../core/core.js"
+
+export function circle(ctx, color, center, radius, width=0, draw_top_right=false, draw_top_left=false, draw_bottom_left=false, draw_bottom_right=false) {
+
+}
