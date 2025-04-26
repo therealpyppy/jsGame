1bb6bbe660d22219055cb807f06bf127aa6e90be
therealpeppy <furghadmanar@gmail.com>
2025-04-25 19:35:11 -0500

    added lines to draw index file

--- src/draw/draw.js
+++ src/draw/draw.js
@@ -1,9 +1,11 @@
 import { rect } from './shapes/rect.js';
 import { line } from './shapes/line.js';
+import { lines } from './shapes/lines.js';
 import { circle } from './shapes/circle.js';
 
 export const draw = {
 	rect,
 	line,
+	lines,
 	circle
 };
