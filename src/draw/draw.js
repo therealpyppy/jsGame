f45ffc4079e1aca6c38e8a091dd6d90eda3b2cad
therealpeppy <furghadmanar@gmail.com>
2025-04-25 20:19:25 -0500

    add polygon to draw index file

--- src/draw/draw.js
+++ src/draw/draw.js
@@ -2,10 +2,12 @@ import { rect } from './shapes/rect.js';
 import { line } from './shapes/line.js';
 import { lines } from './shapes/lines.js';
 import { circle } from './shapes/circle.js';
+import { polygon } from './shapes/polygon.js';
 
 export const draw = {
 	rect,
 	line,
 	lines,
-	circle
+	circle,
+	polygon
 };
