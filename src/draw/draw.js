45d279b437b23ab8a23414760332cce9d64bd394
therealpeppy <furghadmanar@gmail.com>
2025-04-25 20:59:25 -0500

    add ellipse to draw index file

--- src/draw/draw.js
+++ src/draw/draw.js
@@ -3,11 +3,13 @@ import { line } from './shapes/line.js';
 import { lines } from './shapes/lines.js';
 import { circle } from './shapes/circle.js';
 import { polygon } from './shapes/polygon.js';
+import { ellipse } from './shapes/ellipse.js';
 
 export const draw = {
 	rect,
 	line,
 	lines,
 	circle,
-	polygon
+	polygon,
+	ellipse
 };
