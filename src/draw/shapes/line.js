956e1080d02769010d1151683057b2944f04f124
therealpeppy <furghadmanar@gmail.com>
2025-04-25 19:18:34 -0500

    raise vars in file to prevent err

--- src/draw/shapes/line.js
+++ src/draw/shapes/line.js
@@ -1,9 +1,14 @@
 import { core } from "../../core/core.js"
 
 export function line(ctx, color, startPos, endPos, width=1) {
+
+	const rWidth = Math.max(x1, x2) - Math.min(x1, x2);
+	const rHeight = Math.max(y1, y2) - Math.min(y1, y2);
+
 	if (width < 1) {
 		return new core.Rect(Math.min(x1, x2), Math.min(y1, y2), rWidth, rHeight);
 	}
+	
 	ctx.lineWidth = width;
 	ctx.fillStyle = new core.Color(color).hex3;
 	ctx.strokeStyle = new core.Color(color).hex3;
@@ -13,8 +18,6 @@ export function line(ctx, color, startPos, endPos, width=1) {
 	let x2 = endPos[0];
 	let y2 = endPos[1];
 	
-	const rWidth = Math.max(x1, x2) - Math.min(x1, x2);
-	const rHeight = Math.max(y1, y2) - Math.min(y1, y2);
 	ctx.beginPath();
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
