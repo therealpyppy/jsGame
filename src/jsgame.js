80e7667020741905afd229c0b17836b1848a0729
therealpeppy <furghadmanar@gmail.com>
2025-04-26 14:45:25 -0500

    add getInit method

--- src/jsgame.js
+++ src/jsgame.js
@@ -3,28 +3,32 @@ import { draw } from "./draw/draw.js";
 
 class JSGAME {
 	constructor() {
-		this.initialized = false
-
-		this.core = core
-		this.draw = draw
+		this.initialized = false;
+		
+		this.core = core;
+		this.draw = draw;
 	}
-
+	
 	init() {
 		document.body.style.margin = "0px";
 		let canvas = document.querySelector("canvas");
 		if (canvas === null) {
 			let canvas = document.body.appendChild(document.createElement("canvas"));
-			canvas.id = "screen"
+			canvas.id = "screen";
 			canvas.width = window.innerWidth;
 			canvas.height = window.innerHeight;
-
+			
 			return canvas.getContext('2d');
 		}
-		this.initialized = true
+		this.initialized = true;
 	}
-
+	
 	quit() {
-		window.close()
+		window.close();
+	}
+	
+	getInit() {
+		return this.initialized;
 	}
 };
 
