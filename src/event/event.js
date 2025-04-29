9f78858e33c412d7ed90ffd58754728fd3720721
therealpeppy <furghadmanar@gmail.com>
2025-04-28 22:47:28 -0500

    added basic event listeners

--- src/event/event.js
+++ src/event/event.js
@@ -1,3 +1,34 @@
+function addQueue(id) {
+
+}
+
+eventList = new eventList();
+
+visible = true;
+
+window.addEventListener('mousemove', addQueue(id));
+window.addEventListener('mousedown', addQueue(id));
+window.addEventListener('mouseup', addQueue(id));
+window.addEventListener('click', addQueue(id));
+window.addEventListener('dblclick', addQueue(id));
+window.addEventListener('wheel', addQueue(id));
+
+window.addEventListener('keyup', addQueue(id));
+
+window.addEventListener('touchmove', addQueue(id));
+window.addEventListener('touchend', addQueue(id));
+
+document.addEventListener('visibilitychange', (event) => {
+	visible = !visible
+	addQueue(visible ? 513 : 514);
+});
+window.addEventListener('blur', addQueue(1));
+window.addEventListener('focus', addQueue(1));
+
+window.addEventListener('dragstart', addQueue(id));
+window.addEventListener('dragend', addQueue(id));
+window.addEventListener('drop', addQueue(id));
+
 export const event = {
 	
 };
